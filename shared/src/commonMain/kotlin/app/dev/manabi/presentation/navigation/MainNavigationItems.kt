eb04418c274c071f6e117b212bcef5eedac6ae9d
Suraj Verma <surajsays108@zohomail.in>
2026-05-25 23:49:46 +0530

    presentation: update navigation icons to snake_case and remove outlined drawables

--- shared/src/commonMain/kotlin/app/dev/manabi/presentation/navigation/MainNavigationItems.kt
+++ shared/src/commonMain/kotlin/app/dev/manabi/presentation/navigation/MainNavigationItems.kt
@@ -1,9 +1,9 @@
 package app.dev.manabi.presentation.navigation
 
-import manabi.shared.generated.resources.Attendance_filled
-import manabi.shared.generated.resources.Productivity_filled
+import manabi.shared.generated.resources.attendance_filled
+import manabi.shared.generated.resources.productivity_filled
 import manabi.shared.generated.resources.Res
-import manabi.shared.generated.resources.Schedule_filled
+import manabi.shared.generated.resources.schedule_filled
 import org.jetbrains.compose.resources.DrawableResource
 
 data class NavigationItem(
@@ -17,20 +17,20 @@ val navigationItems = listOf(
     NavigationItem(
         screen = Screen.MainGraph.Attendance,
         label = "Attendance",
-        selectedIcon = Res.drawable.Attendance_filled,
-        unselectedIcon = Res.drawable.Attendance_filled,
+        selectedIcon = Res.drawable.attendance_filled,
+        unselectedIcon = Res.drawable.attendance_filled,
     ),
     NavigationItem(
         screen = Screen.MainGraph.Productivity,
         label = "Productivity",
-        selectedIcon = Res.drawable.Productivity_filled,
-        unselectedIcon = Res.drawable.Productivity_filled,
+        selectedIcon = Res.drawable.productivity_filled,
+        unselectedIcon = Res.drawable.productivity_filled,
     ),
     NavigationItem(
         screen = Screen.MainGraph.Schedule,
         label = "Schedule",
-        selectedIcon = Res.drawable.Schedule_filled,
-        unselectedIcon = Res.drawable.Schedule_filled,
+        selectedIcon = Res.drawable.schedule_filled,
+        unselectedIcon = Res.drawable.schedule_filled,
     ),
 )
 
